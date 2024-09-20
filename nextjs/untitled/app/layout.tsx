a400a6b960867a89ae8d070b709563d5aaef4656
ParkSec <ppssbb1024@gmail.com>
2024-09-20 09:56:52 +0900

    first commit

---
+++ nextjs/untitled/app/layout.tsx
@@ -0,0 +1,40 @@
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
+import "../styles/globals.css";
+import "../styles/text-editor.css";
+import { Suspense } from "react";
+import { Providers } from "./Providers";
+
+export const metadata = {
+    title: "Liveblocks",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+        <head>
+            <link
+                href="https://liveblocks.io/favicon-32x32.png"
+                rel="icon"
+                sizes="32x32"
+                type="image/png"
+            />
+            <link
+                href="https://liveblocks.io/favicon-16x16.png"
+                rel="icon"
+                sizes="16x16"
+                type="image/png"
+            />
+        </head>
+        <body>
+        <Providers>
+            <Suspense>{children}</Suspense>
+        </Providers>
+        </body>
+        </html>
+    );
+}
